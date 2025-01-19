037add54dc2d8cdfe88e394a17624f1824984afa
SlickBuy <slickbuy54@gmail.com>
2025-01-19 15:21:13 -0800

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,26 @@
-'use client'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ThemeRegistry from './ThemeRegistry'
-import Footer from './components/footer/footer'
-import NavBar from './components/navbar'
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ThemeRegistry from "./ThemeRegistry";
+import Footer from "./components/footer/footer";
+import NavBar from "./components/navbar";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Supplement store',
-  description: 'Supplement store',
-}
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}> 
-        <ThemeRegistry options={{ key: 'mui' }}>
+      <body className={inter.className}>
+        <ThemeRegistry options={{ key: "mui" }}>
           <NavBar />
-          <main>
-            {children}
-          </main>
+          <main>{children}</main>
           <Footer />
         </ThemeRegistry>
       </body>
     </html>
-  )
+  );
 }
